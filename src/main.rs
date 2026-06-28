94087622be63d2e13b6b9d534a73570401e2dcf4
micts <tsiaousis.michail@gmail.com>
2026-06-28 22:42:04 +0200

    Add function and test for search case insensitive; Add case insensitive option as an environment variable

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     
@@ -24,7 +24,13 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -32,8 +38,10 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+
 }
 
 impl Config {
@@ -46,6 +54,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { 
+            query, 
+            file_path,
+            ignore_case,
+        })
     }
 }
